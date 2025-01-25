3fced2e5b278fdcab48a0adceb2194065219eb39
Wadson <wadsonwadson@hotmail.com>
2025-01-25 18:40:26 -0300

    Novo SistemaSisControl Pronto

---
+++ Relatorios/Relatorio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SisControl.Relatorios
+{
+    public partial class Relatorio : SisControl.FrmModeloForm
+    {
+        public Relatorio()
+        {
+            InitializeComponent();
+        }
+    }
+}
